c8d4cea80f705cb2e89130c9f2292ae5d2a61de4
chelsejw <chelsejw@gmail.com>
2020-08-21 00:41:25 +0800

    basic show route for tweet

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
+from tweets.models import Tweet
 
 # Create your views here.
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    return HttpResponse("<h1>Hello world!!</h1>")
+0
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    print(args, kwargs)
+    status = 200
+    data = {
+        "id": tweet_id,
+    }
+    try:
+        tweet = Tweet.objects.get(id=tweet_id)
+        data['content'] = tweet.content
+    except:
+        data['message'] = "Not found"
+        status = 404
+    return JsonResponse(data, status=status)
